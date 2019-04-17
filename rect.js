1dda5f722a792f4e79198268109abb9e5d4cf214
charley <42422198+charleykitty@users.noreply.github.com>
2019-04-17 11:36:14 -1000

    Update rect.js

--- rect.js
+++ rect.js
@@ -1,4 +1,35 @@
     function obst() {
+      this.rand = Math.floor(Math.random() * 700);
+      this.x = w;
+      this.y = h - this.rand;
+      this.gravity = 1;
+      this.velocity = 0;
+    
+      
+      this.show = function() {
+        fill(color('white'));
+        rect(this.x,this.y,220,20);
+      
+   }
+   this.update = function() {
+    this.velocity += this.gravity; //gravity applied when not jumping
+    this.x -= this.velocity;
+    if(this.x < 0){
+      this.rand = Math.floor(Math.random() * 700);
+      this.x = w;
+      this.y = h - this.rand;
+      this.velocity = 0;
+      number += 1;
+      
+    }
+    if(hit === true) {
+     noLoop();
+   }
+   }
+ }
+ //object ends here
+
+function obst1() {
       this.rand = Math.floor(Math.random() * 700)
       this.x = w;
       this.y = h - this.rand;
@@ -19,13 +50,11 @@
       this.x = w;
       this.y = h - this.rand;
       this.velocity = 0;
-      
+      //number += 1;
     }
-   }
- this.kill = function() {
-   if(hit === true) {
+    if(hit2 === true) {
      noLoop();
    }
- }
+   }
  }
  //object ends here
